4590a7c83cc66d5fe044815cd20615423fce7fbe
CIKADA <adel.garipovazn@mail.ru>
2023-05-17 12:37:29 +0300

    Add burger script

---
+++ scripts/burger.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".header__burger");
+if (burger) {
+  const menuBody = document.querySelector(".menu__body");
+  burger.addEventListener("click", () => {
+    document.body.classList.toggle("body_lock");
+    burger.classList.toggle("header__burger_active");
+    menuBody.classList.toggle("menu__body_active");
+  });
+}
