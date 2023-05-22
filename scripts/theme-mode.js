c1db62509fe3179929a9364188e11dfd5751df5b
CIKADA <adel.garipovazn@mail.ru>
2023-05-22 17:03:46 +0300

    Add theme switcher

---
+++ scripts/theme-mode.js
@@ -0,0 +1,16 @@
+// const body = document.documentElement();
+
+const themeButtons = document.querySelectorAll(".theme__button");
+
+themeButtons.forEach((btnItem) => {
+  btnItem.addEventListener("click", (event) => {
+    document.documentElement.classList.remove("light", "dark");
+    if (event.target.classList.contains("theme__button_dark-mode")) {
+      document.documentElement.classList.add("light");
+    }
+    if (event.target.classList.contains("theme__button_light-mode")) {
+      document.documentElement.classList.add("dark");
+    }
+    //ToDO можно сохранить в localStorage цвет темы
+  });
+});
