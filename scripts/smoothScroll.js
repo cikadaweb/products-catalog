6ca69ba3e708a450c331addf8bb99ab3e799e054
CIKADA <adel.garipovazn@mail.ru>
2023-05-26 23:28:25 +0300

    Add burger close

--- scripts/smoothScroll.js
+++ scripts/smoothScroll.js
@@ -1,9 +1,17 @@
 const smoothScroll = () => {
   const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+  const burger = document.querySelector(".header__burger");
+  const menuBody = document.querySelector(".menu__body");
+
   if (menuLinks.length > 0) {
     menuLinks.forEach((menuLink) => {
       menuLink.addEventListener("click", (event) => {
         event.preventDefault();
+
+        document.body.classList.toggle("body_lock");
+        burger.classList.toggle("header__burger_active");
+        menuBody.classList.toggle("menu__body_active");
+
         const menuLink = event.target;
         if (
           menuLink.dataset.goto &&
