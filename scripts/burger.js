ec28dedd06fabb329e20cb61d87d83bc1af44a90
CIKADA <adel.garipovazn@mail.ru>
2023-05-27 02:55:50 +0300

    Refactor modal aboutme

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,11 +1,18 @@
 const burger = () => {
   const burger = document.querySelector(".header__burger");
+  const aboutMeModalOpen = document.querySelector(".menu__link_aboutme");
+
   if (burger) {
     const menuBody = document.querySelector(".menu__body");
     burger.addEventListener("click", () => {
       document.body.classList.toggle("body_lock");
       burger.classList.toggle("header__burger_active");
       menuBody.classList.toggle("menu__body_active");
+    });
+    aboutMeModalOpen.addEventListener("click", () => {
+      document.body.classList.remove("body_lock");
+      burger.classList.remove("header__burger_active");
+      menuBody.classList.remove("menu__body_active");
     });
   }
 };
