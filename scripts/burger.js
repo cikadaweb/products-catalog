0357a9c4dd1447a0c8305d9855241b6bcf703bb0
CIKADA <adel.garipovazn@mail.ru>
2023-05-26 23:26:24 +0300

    Add closure

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,13 @@
-const burger = document.querySelector(".header__burger");
-if (burger) {
-  const menuBody = document.querySelector(".menu__body");
-  burger.addEventListener("click", () => {
-    document.body.classList.toggle("body_lock");
-    burger.classList.toggle("header__burger_active");
-    menuBody.classList.toggle("menu__body_active");
-  });
-}
+const burger = () => {
+  const burger = document.querySelector(".header__burger");
+  if (burger) {
+    const menuBody = document.querySelector(".menu__body");
+    burger.addEventListener("click", () => {
+      document.body.classList.toggle("body_lock");
+      burger.classList.toggle("header__burger_active");
+      menuBody.classList.toggle("menu__body_active");
+    });
+  }
+};
+
+burger();
