02d68d4fb3bccb5ff3b6b0cba98b1e4b61d45ffc
CIKADA <adel.garipovazn@mail.ru>
2023-05-26 17:59:15 +0300

    Refactor smooth scroll

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,9 +1,7 @@
 const smoothScroll = () => {
-  const headerLinks = document.querySelectorAll(".menu__link");
+  const menuLinks = document.querySelectorAll(".menu__link");
 
-  console.log(headerLinks);
-
-  headerLinks.forEach((link) => {
+  menuLinks.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
       const ID = event.target.getAttribute("href").substr(1);
