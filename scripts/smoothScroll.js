f88fd04b9bb4ffa9063f3061de125cf199016ce2
CIKADA <adel.garipovazn@mail.ru>
2023-06-18 13:31:04 +0300

    Refactor smoothScroll

--- scripts/smoothScroll.js
+++ scripts/smoothScroll.js
@@ -1,7 +1,9 @@
 const smoothScroll = () => {
   const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+
   const burger = document.querySelector(".header__burger");
   const menuBody = document.querySelector(".menu__body");
+
   const logoLink = document.querySelector(".header__logo");
   const allLinks = [...menuLinks, logoLink];
 
@@ -10,11 +12,9 @@ const smoothScroll = () => {
       menuLink.addEventListener("click", (event) => {
         event.preventDefault();
 
-        //
-        document.body.classList.remove("body_lock");
-        burger.classList.remove("header__burger_active");
-        menuBody.classList.remove("menu__body_active");
-        //
+        if (burger.classList.contains("header__burger_active  ")) {
+          resetBurgerState();
+        }
 
         const menuLink = event.target;
         if (
@@ -26,7 +26,7 @@ const smoothScroll = () => {
             gotoBlock.getBoundingClientRect().top +
             pageYOffset -
             document.querySelector("header").offsetHeight -
-            50; // положение с учетом header
+            50;
 
           window.scrollTo({
             top: gotoBlockValue,
@@ -35,6 +35,11 @@ const smoothScroll = () => {
         }
       });
     });
+  }
+  function resetBurgerState() {
+    document.body.classList.remove("body_lock");
+    burger.classList.remove("header__burger_active");
+    menuBody.classList.remove("menu__body_active");
   }
 };
 
